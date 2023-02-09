5cda592daccf35028e30b648d58cc30e9612da07
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-02-08 19:12:16 -0800

    added PizzaContext class

---
+++ PizzaRe-up/PizzaContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaRe_up
+{
+    internal class PizzaContext : DbContext
+    {
+        public PizzaContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EFCoreExample;Trusted_Connection=True");
+        }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+    }
+}
