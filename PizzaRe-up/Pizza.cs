6668ecc5ac2539e330401fc65888918cc19305d9
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-02-08 19:23:31 -0800

    add key annotation to pizza class

--- PizzaRe-up/Pizza.cs
+++ PizzaRe-up/Pizza.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace PizzaRe_up
         /// <summary>
         /// Ingredients for the pizza(s).
         /// </summary>
+        [Key]
         public string Ingredients { get; set; }
 
         /// <summary>
