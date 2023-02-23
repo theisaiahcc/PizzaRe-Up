25b7494f905988ced12d275e906c5716ea304c07
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-02-23 13:46:28 -0800

    Added a message box saying order completed when customer presses submit.

--- PizzaRe-up/Form1.cs
+++ PizzaRe-up/Form1.cs
@@ -55,7 +55,8 @@ namespace PizzaRe_up
                 Pizza p = new Pizza(ingredients, sauce, price, crust, size, name);
                 DbContext.Add(p);
             }
-            
+            MessageBox.Show("Your order has been completed");
+
 
             // Adds 1 to price for each ingredient and concatenates ingredients string
             void getIngredients(ref string ingredients, ref double price)
