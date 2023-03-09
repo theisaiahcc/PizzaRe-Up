33fff87927a8a063be2fb18d0a8d8b4dae97df88
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-03-08 16:03:22 -0800

    Update now updates selected object

--- PizzaRe-up/PizzaContext.cs
+++ PizzaRe-up/PizzaContext.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +20,22 @@ namespace PizzaRe_up
         {
             using PizzaContext DbContext = new();
             DbContext.Pizzas.Add(p);
+            DbContext.SaveChanges();
+        }
+
+        public void Update(Pizza p, int id)
+        {
+            using PizzaContext DbContext = new PizzaContext();
+            Pizza pizza = DbContext.Pizzas.Find(id);
+
+            Type type = typeof(Pizza);
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                if (property.Name != "PizzaId") {
+                    DbContext.Entry(pizza).Property(property.Name).CurrentValue = property.GetValue(p);
+                }
+            }
             DbContext.SaveChanges();
         }
 
